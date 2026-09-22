e33747316527cbbd06f3d49d787291fe23105124
lyhout-hak <lyhout-hak@mptc.gov.kh>
2026-09-22 10:25:56 +0700

    feat: create BusinessPersistenceException in common-persistence and OrderGlobalExceptionHandler in order-service-restapi

---
+++ common/common-persistence/src/main/java/co/istad/lyhout/ecommerce/persistence/exception/BusinessPersistenceException.java
@@ -0,0 +1,7 @@
+package co.istad.lyhout.ecommerce.persistence.exception;
+
+public class BusinessPersistenceException extends RuntimeException {
+    public BusinessPersistenceException(String message) {
+        super(message);
+    }
+}
--- order-service/order-service-restapi/src/main/java/co/istad/lyhout/ecommerce/order/restapi/exception/OrderGlobalExceptionHandler.java
+++ order-service/order-service-restapi/src/main/java/co/istad/lyhout/ecommerce/order/restapi/exception/OrderGlobalExceptionHandler.java
@@ -1,9 +1,32 @@
 package co.istad.lyhout.ecommerce.order.restapi.exception;
 
+import co.istad.lyhout.ecommerce.order.domain.exception.OrderDomainException;
+import co.istad.lyhout.ecommerce.persistence.exception.BusinessPersistenceException;
+import co.istad.lyhout.ecommerce.restapi.dto.RestApiErrorResponse;
 import co.istad.lyhout.ecommerce.restapi.exception.GlobalExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class OrderGlobalExceptionHandler extends GlobalExceptionHandler {
-    // TODO WRITE YOUR EXCEPTION HANDLER HERE
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(OrderDomainException.class)
+    public RestApiErrorResponse<?> handleOrderDomainException(OrderDomainException e) {
+        return RestApiErrorResponse.builder()
+                .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(BusinessPersistenceException.class)
+    public RestApiErrorResponse<?> handleOrderPersistenceException(BusinessPersistenceException e) {
+        return RestApiErrorResponse.builder()
+                .code(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .message(e.getMessage())
+                .build();
+    }
 }
